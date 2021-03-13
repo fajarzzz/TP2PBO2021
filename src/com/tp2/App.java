377473d67270c811c638b94048965ef53b768cbd
fajarzzz <fajarzuliansyaht@upi.edu>
2021-03-13 23:20:10 +0700

    Connect and Insert Data to Database Success

--- src/com/tp2/App.java
+++ src/com/tp2/App.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.*;
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
 
@@ -35,14 +36,14 @@ public class App {
     private JLabel showImage;
     private JLabel nimLb;
 
-    ArrayList<Mobil> arrMobil;
     int row, col;
     DefaultTableModel dtm;
     String[] header = new String[]{"No", "Merk", "Plat", "Warna", "Jenis"};
+    Connection connect;
 
-    public App(){
+    public App() {
         createUIComponents();
-        arrMobil = new ArrayList<>();
+        connectDB();
         this.namaLb.setText("Nama : Fajar Zuliansyah Trihutama");
         this.nimLb.setText("NIM   : 1905394");
         dtm = new DefaultTableModel(header, 0);
@@ -83,6 +84,43 @@ public class App {
                 DynamicContainer.revalidate();
             }
         });
+        submitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String merk = merkTb.getText();
+                String plat = platTb.getText();
+                String warna = warnaTb.getText();
+                String jenis = jenisBox.getSelectedItem().toString();
+
+                if(merk.equals("") || plat.equals("") || warna.equals("") || jenis.equals("")){
+                    JOptionPane.showMessageDialog(null, "Tidak boleh ada data yang kosong");
+                } else {
+                    try {
+                        PreparedStatement preparedStatement = connect.prepareStatement("INSERT INTO mobil" + "  (merk, plat, warna, jenis) VALUES " + " (?, ?, ?, ?);");
+                        preparedStatement.setString(1, merk);
+                        preparedStatement.setString(2, plat);
+                        preparedStatement.setString(3, warna);
+                        preparedStatement.setString(4, jenis);
+                        preparedStatement.executeUpdate();
+                    } catch (SQLException sqlException){
+                        sqlException.printStackTrace();
+                    }
+                    merkTb.setText("");
+                    platTb.setText("");
+                    warnaTb.setText("");
+                }
+            }
+        });
+    }
+
+    public void connectDB() {
+        try{
+            this.connect = (Connection) DriverManager
+                    .getConnection("jdbc:mysql://localhost:3306/tp2_java", "root", "");
+        } catch(SQLException sqlException){
+            sqlException.printStackTrace();
+        }
+
     }
     public static void main(String[] args){
         JFrame frame = new JFrame("App");
@@ -95,5 +133,25 @@ public class App {
     private void createUIComponents() {
         // TODO: place custom component creation code here
         showImage = new JLabel(new ImageIcon("image\\PAS FOTO BEBAS.jpg"));
+        dataTabel = new JTable();
+
+        dataTabel.setBackground(new Color(225,225,225));
+        dataTabel.setBorder(new javax.swing.border.MatteBorder(null));
+        dataTabel.setModel(new DefaultTableModel(
+                new Object [][] {
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null},
+                        {null, null, null, null}
+                },
+                new String [] {
+                        "Title 1", "Title 2", "Title 3", "Title 4", "Title 5"
+                }
+        ));
+
+        jenisBox = new JComboBox<>();
+        jenisBox.setModel(new DefaultComboBoxModel<>(new String[] { "Biasa", "Balap", "Sport", "Truk" }));
+
+
     }
 }
